c8f88ca9ac4f84220d982f28b8fdc461551fb01c
AngelNovo <angelnovo15@gmail.com>
2022-08-25 22:54:10 +0200

    Update math_fun.py

--- lib/math_fun.py
+++ lib/math_fun.py
@@ -1,18 +1,27 @@
 import lib.functionalities as fun
 import lib.memory as me
 
+def unpack(options):
+    values = me.checkVariable(options['v'])
+
+    fn = options['f']
+    return values, fn
+
 def math_doing(f, v):
     if f=='print':
         fun.p_msg(v)
 
 
 def m_sum(options):
-    values = me.checkVariable(options['v'])
 
-    fn = options['f']
+    values, fn = unpack(options)
+
     total = 0
     
     for v in values:
         total += int(v)
     math_doing(fn, total)
+    
+def m_sub(options):
+    pass
 
